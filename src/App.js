901b0abcaa7e6781b8320175ef9ecb3af330bf09
Chiranjeevi Makam <137588934+chiranjeevi-makam@users.noreply.github.com>
2024-07-30 15:53:07 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {Component} from 'react' 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class APP extends Component{ 
+  
+  state={status:true} 
+
+  change=()=>{
+    
+    this.setState((prev)=>({status:!prev.status})) 
+    
+  }
+
+  render(){  
+    const{status}=this.state 
+    console.log(status)
+    return( 
+    <div className='welcomeBackground'> 
+        <h1 className='textColor'>Welcome</h1>  
+        <p className='paraColor'>Thank you ! Happy Learning!</p> 
+        
+        <br/>
+        <button  onClick={this.change}>{status?"Subscribe":"subscribed"}</button>
+          
     </div>
-  );
+      
+    )
+  }
 }
-
-export default App;
+export default APP
